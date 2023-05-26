7e08f11414c96c77c62c60806b7acbd82dc07027
Ribosome2 <583154233@qq.com>
2023-05-26 22:19:49 +0800

    修复Annator运行时候的报错

--- kyle-lox/src/main/java/org/intellij/sdk/language/LoxAnnotator.java
+++ kyle-lox/src/main/java/org/intellij/sdk/language/LoxAnnotator.java
@@ -1,19 +1,22 @@
-// Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.intellij.sdk.language;
 
-import com.intellij.lang.annotation.*;
-import com.intellij.openapi.project.Project;
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLiteralExpression;
 import org.intellij.sdk.language.psi.LoxProperty;
 import org.jetbrains.annotations.NotNull;
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 
 import java.util.List;
 
-
 public class LoxAnnotator implements Annotator {
+
     // Define strings for the Lox language prefix - used for annotations, line markers, etc.
     public static final String LOX_PREFIX_STR = "lox";
     public static final String LOX_SEPARATOR_STR = ":";
@@ -21,12 +24,16 @@ public class LoxAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
         // Ensure the Psi Element is an expression
-        if (!(element instanceof PsiLiteralExpression)) return;
+        if (!(element instanceof PsiLiteralExpression)) {
+            return;
+        }
 
-        // Ensure the Psi element contains a string that starts with the key and separator
+        // Ensure the Psi element contains a string that starts with the prefix and separator
         PsiLiteralExpression literalExpression = (PsiLiteralExpression) element;
         String value = literalExpression.getValue() instanceof String ? (String) literalExpression.getValue() : null;
-        if ((value == null) || !value.startsWith(LOX_PREFIX_STR + LOX_SEPARATOR_STR)) return;
+        if ((value == null) || !value.startsWith(LOX_PREFIX_STR + LOX_SEPARATOR_STR)) {
+            return;
+        }
 
         // Define the text ranges (start is inclusive, end is exclusive)
         // "lox:key"
@@ -35,26 +42,27 @@ public class LoxAnnotator implements Annotator {
         TextRange separatorRange = TextRange.from(prefixRange.getEndOffset(), LOX_SEPARATOR_STR.length());
         TextRange keyRange = new TextRange(separatorRange.getEndOffset(), element.getTextRange().getEndOffset() - 1);
 
-        // Get the list of properties from the Project
-        String possibleProperties = value.substring(LOX_PREFIX_STR.length() + LOX_SEPARATOR_STR.length());
-        Project project = element.getProject();
-        List<LoxProperty> properties = LoxUtil.findProperties(project, possibleProperties);
+        // highlight "lox" prefix and ":" separator
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .range(prefixRange).textAttributes(DefaultLanguageHighlighterColors.KEYWORD).create();
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .range(separatorRange).textAttributes(LoxSyntaxHighlighter.SEPARATOR).create();
 
-        // Set the annotations using the text ranges.
-        Annotation keyAnnotation = holder.createInfoAnnotation(prefixRange, null);
-        keyAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.KEYWORD);
-        Annotation separatorAnnotation = holder.createInfoAnnotation(separatorRange, null);
-        separatorAnnotation.setTextAttributes(LoxSyntaxHighlighter.SEPARATOR);
+
+        // Get the list of properties for given key
+        String key = value.substring(LOX_PREFIX_STR.length() + LOX_SEPARATOR_STR.length());
+        List<LoxProperty> properties = LoxUtil.findProperties(element.getProject(), key);
         if (properties.isEmpty()) {
-            // No well-formed property found following the key-separator
-            Annotation badProperty = holder.createErrorAnnotation(keyRange, "Unresolved property");
-            badProperty.setTextAttributes(LoxSyntaxHighlighter.BAD_CHARACTER);
-            // ** Tutorial step 18.3 - Add a quick fix for the string containing possible properties
-//            badProperty.registerFix(new LoxCreatePropertyQuickFix(possibleProperties));
+            holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved property")
+                    .range(keyRange)
+                    .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
+                    // ** Tutorial step 18.3 - Add a quick fix for the string containing possible properties
+//                    .withFix(new LoxCreatePropertyQuickFix(key))
+                    .create();
         } else {
-            // Found at least one property
-            Annotation annotation = holder.createInfoAnnotation(keyRange, null);
-            annotation.setTextAttributes(LoxSyntaxHighlighter.VALUE);
+            // Found at least one property, force the text attributes to Lox syntax value character
+            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(keyRange).textAttributes(LoxSyntaxHighlighter.VALUE).create();
         }
     }
 
